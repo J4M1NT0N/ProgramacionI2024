7497f6e4d410d2f2ba2b4b740f521425b4e97a56
J4M1NT0N <135446614+J4M1NT0N@users.noreply.github.com>
2024-03-02 10:51:23 -0600

    Create ejercicio2.cs

---
+++ PrimerParcial/ejercicio2.cs
@@ -0,0 +1,11 @@
+using System;
+public class CantNumeros
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la cantidad de numeros que desea ingresar:");
+        double cantnums = Convert.ToDouble(Console.ReadLine());
+    }
+
+    
+}
